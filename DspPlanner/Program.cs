4ea7d55599bdf5944f345d04a81f80d2646f0d77
Alex Davidson <alda@epro.com>
2022-03-08 18:00:18 +0000

    Basic project structure

---
+++ DspPlanner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DspPlanner
+{
+    internal class Program
+    {
+        internal static async Task<int> Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
